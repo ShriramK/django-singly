df61ced47c5f1c72abfdcd65bfb46df8f4d32f73
Justin Parker <foggysmoke@gmail.com>
2012-12-06 10:55:36 -0800

    update to use /authenticate url and pattern

--- singly/templatetags/singly.py
+++ singly/templatetags/singly.py
@@ -6,16 +6,17 @@ from ..views import callback
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def singly_login_url(context, service):
     redirect_uri = getattr(settings, 'SINGLY_CALLBACK_URL', None)
     if redirect_uri is None:
         redirect_uri = "http://%s%s" % (context['request'].get_host(), reverse(callback, current_app='singly'))
-    auth_url = "https://api.singly.com/oauth/authorize?client_id=%s&redirect_uri=%s&service=%s" % (
+    auth_url = "https://api.singly.com/oauth/authenticate?client_id=%s&redirect_uri=%s&service=%s" % (
         getattr(settings, 'SINGLY_CLIENT_ID', None),
         redirect_uri,
         service
     )
     if hasattr(context['user'], 'singly'):
-        auth_url = "%s&account=%s" % (auth_url, context['user'].singly.account)
+        auth_url = "%s&access_token=%s" % (auth_url, context['user'].singly.access_token)
     return auth_url
